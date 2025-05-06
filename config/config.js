dfc93f2da7f1c2b2d51ca789e29f8f1112da6e63
JulBrandao <141237068+julbrandao@users.noreply.github.com>
2025-05-06 08:57:47 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,34 +1,23 @@
-require('dotenv').config();
-
-module.exports = {
-  development: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+{
+  "development": {
+    "username": "root",
+    "password": "",
+    "database": "3dspi",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
-  test: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+  "test": {
+    "username": "root",
+    "password": "",
+    "database": "3dspi",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
-  production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+  "production": {
+    "username": "root",
+    "password": "",
+    "database": "3dspi",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   }
-};
+}
